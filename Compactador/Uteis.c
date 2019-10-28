72b7f5afa0c7f2547754c3023508d734301aac4f
Scherer <fsvicentin@hotmail.com>
2019-10-28 13:27:29 -0300

    Rotação da barra e caracter de preenchimento modificado

--- Compactador/Uteis.c
+++ Compactador/Uteis.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "Uteis.h"
 
 No* novoNo()
@@ -32,6 +33,9 @@ void avancarSimboloBarra(Barra *b)
         case '\\':
             b->atual = '|';
             break;
+        default:
+            b->atual = '|';
+            break;
     }
 }
 
@@ -42,12 +46,17 @@ void setMaxPorcentagem(Barra *b, unsigned int m)
 
 void setPorcentagem(Barra *b, unsigned int p)
 {
+    static int k = 0;
     int d = b->porcentagem[1];
     float f = (float)p / d;
     f *= 100;
+    if (f >= k * 0.005f)
+    {
+        avancarSimboloBarra(b);
+        k++;
+    }
     b->porcentagem[0] = (unsigned int)f;
     printarStatus(*b);
-    avancarSimboloBarra(b);
 }
 
 void printarStatus(Barra b)
@@ -55,16 +64,19 @@ void printarStatus(Barra b)
     static char status[5][16] = {"LENDO", "MONTANDO FILA", "MONTANDO ARVORE", "OBTENDO CODIGOS", "ESCREVENDO"};
     char *barra = (char*)malloc(150 * sizeof(char));
     char *parte = (char*)malloc((strlen(status[b.parteAtual]) + 3) * sizeof(char));
+    char *preenchimento = (char*)malloc(2 * sizeof(char));
     int i;
+    preenchimento[1] = '\0';
+    preenchimento[0] = 178;
     strcpy(barra, "\0");
     strcpy(parte, "[");
     strcat(parte, status[b.parteAtual]);
     strcat(parte, "]");
-    for (i = 0; i < b.porcentagem[0]/10; i++)
-        strcat(barra, "X");
+    for (i = 0; i < b.porcentagem[0]/4; i++)
+        strcat(barra, preenchimento);
     limparLinha();
 
-    printf("%-18s %c %d%% |%-10s|", parte, b.atual, b.porcentagem[0], barra);
+    printf("%-18s %c %d%% |%-25s|", parte, b.atual, b.porcentagem[0], barra);
 
     free(barra);
     free(parte);
